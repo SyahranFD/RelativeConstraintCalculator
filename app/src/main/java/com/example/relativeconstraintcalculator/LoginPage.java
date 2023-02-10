2eb1170c66183fa1262678c6ba62e4802ae57a5e
SyahranFD <fadhilrafa1@gmail.com>
2023-02-10 16:43:23 +0700

    feat : [SF] add login onclick

--- app/src/main/java/com/example/relativeconstraintcalculator/LoginPage.java
+++ app/src/main/java/com/example/relativeconstraintcalculator/LoginPage.java
@@ -2,13 +2,91 @@ package com.example.relativeconstraintcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.relativeconstraintcalculator.databinding.ActivityLoginPageBinding;
+
 
 public class LoginPage extends AppCompatActivity {
+
+    private ActivityLoginPageBinding binding;
+
+    // creating constant keys for shared preferences.
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    // key for storing email.
+    public static final String EMAIL_KEY = "email_key";
+
+    // key for storing password.
+    public static final String PASSWORD_KEY = "password_key";
+
+    // variable for shared preferences.
+    SharedPreferences sharedpreferences;
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edUsername = binding.edUsername;
+        EditText edPassword = binding.edPassword;
+        Button btnLogin = binding.btnLogin;
+
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        email = sharedpreferences.getString(EMAIL_KEY, null);
+        password = sharedpreferences.getString(PASSWORD_KEY, null);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("admin")){
+
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                    // below two lines will put values for
+                    // email and password in shared preferences.
+                    editor.putString(EMAIL_KEY, edUsername.getText().toString());
+                    editor.putString(PASSWORD_KEY, edPassword.getText().toString());
+
+                    // to save our data with key and value.
+                    editor.apply();
+
+//                    Intent calculator = new Intent(LoginPage.this,Calculator.class);
+//                    startActivity(calculator);
+//                    finish();
+
+                }else{
+
+                    new AlertDialog.Builder(LoginPage.this)
+                            .setTitle("Gagal Login")
+                            .setMessage("Username atau Password Salah")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            }
+        });
+
     }
 }
